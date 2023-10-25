d41f52831eb18daa0afadcbd85c0034521109ef5
Jean Carlos <140626458+JeanCaarls@users.noreply.github.com>
2023-10-25 12:17:11 -0300

    Update db.js
    removendo senha

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("test", "root", "@JJee112233", {
+const sequelize = new Sequelize("test", "root", "######", {
     host: "localhost",
     dialect: "mysql"
 });
